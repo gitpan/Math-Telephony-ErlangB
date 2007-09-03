5f86e682a099b3982e1714965ade410a1d39ab3d
Flavio Poletti <polettix@cpan.org>
2007-09-03 16:37:41 +0000

    Import of POLETTIX/Math-Telephony-ErlangB-0.9.1 from CPAN.
    gitpan-cpan-distribution: Math-Telephony-ErlangB
    gitpan-cpan-version:      0.9.1
    gitpan-cpan-path:         POLETTIX/Math-Telephony-ErlangB-0.9.1.tar.gz
    gitpan-cpan-author:       POLETTIX
    gitpan-cpan-maturity:     released

--- eg/gos.pl
+++ eg/gos.pl
@@ -5,7 +5,7 @@ use warnings;
 use Math::Telephony::ErlangB qw( gos );
 
 $|++;
-print "traffic: "; chomp(my $traffic = <STDIN>);
-print "servers: "; chomp(my $servers = <STDIN>);
+print "traffic (Erlang): "; chomp(my $traffic = <STDIN>);
+print "servers (number): "; chomp(my $servers = <STDIN>);
 printf "blocking probability for $servers servers and $traffic Erlang of traffic is %.1f%%\n", gos($traffic, $servers) * 100;
 
