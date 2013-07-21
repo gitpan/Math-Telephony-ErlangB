8de52a70af817f7cc0940fbb235e17acd44ca5b5
Flavio Poletti <polettix@cpan.org>
2013-07-21 14:05:26 +0000

    Import of POLETTIX/Math-Telephony-ErlangB-1.0.2 from CPAN.
    gitpan-cpan-distribution: Math-Telephony-ErlangB
    gitpan-cpan-version:      1.0.2
    gitpan-cpan-path:         POLETTIX/Math-Telephony-ErlangB-1.0.2.tar.gz
    gitpan-cpan-author:       POLETTIX
    gitpan-cpan-maturity:     released

--- lib/Math/Telephony/ErlangB.pm
+++ lib/Math/Telephony/ErlangB.pm
@@ -1,6 +1,6 @@
 package Math::Telephony::ErlangB;
 
-use version; our $VERSION = qv('1.0.1');
+use version; our $VERSION = qv('1.0.2');
 
 use warnings;
 use strict;
@@ -133,6 +133,8 @@ sub traffic {
 
 1;    # Magic true value required at end of module
 __END__
+
+=encoding iso-8859-1
 
 =head1 NAME
 
